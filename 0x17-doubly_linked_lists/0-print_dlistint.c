3a12106e2f07ed0b6bfc2903d24ae178e9e7631e
JJHannah <zemjefferyzem@gmail.com>
2022-12-08 20:31:16 +0000

    inital commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,9 +12,9 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *temp;
 	
 	temp = h;
-	while (h)
+	while (temp)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", temp->n);
 		temp = temp->next;
 		nodes++;
 	}
